4d589a9316e184cc1bd5128623ff686220692f28
Duck1s <32653@ma-web.nl>
2022-09-20 14:27:29 +0200

    feat(app): make appjs and add it to indexjs and make indexjs and remove unused files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Import React
+import React from "react"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Import ReactDOM
+import ReactDOM from "react-dom/client"
+
+// Import App
+import App from "./App"
+
+// Variable for root
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+// Render variable
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+)
