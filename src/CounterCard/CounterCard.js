fcd8a265748bdc65cda3c1e00f7d8749db95a6d5
Duck1s <32653@ma-web.nl>
2022-09-20 15:00:54 +0200

    feat(countercard): add header and styles to value and buttons

--- src/CounterCard/CounterCard.js
+++ src/CounterCard/CounterCard.js
@@ -6,7 +6,9 @@ const CounterCard = () => {
 	return (
 		<>
 			<article className='counter'>
-				<section className='counter__number'>0</section>
+				<section className='counter__number'>
+					<h1 className='counter__value'>0</h1>
+				</section>
 				<section className='counter__buttons'>
 					<button className='counter__button'>Decrease</button>
 					<button className='counter__button'>Increase</button>
