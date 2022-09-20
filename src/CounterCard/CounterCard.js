a7dc38277ca7a0ee5fcc6a4e9d47c8a1e1a7e1d4
Duck1s <32653@ma-web.nl>
2022-09-20 15:38:48 +0200

    feat(countercard): switch to class component and add some class comp features

--- src/CounterCard/CounterCard.js
+++ src/CounterCard/CounterCard.js
@@ -1,37 +1,48 @@
 // Imports
 import "./CounterCard.css"
+import React from "react"
 
 // Functions
-const CounterCard = (props) => {
-	const increase = () => {
+class CounterCard extends React.Component {
+	constructor() {
+		super(this.props)
+		this.state = {
+			voornaam: "Joeri",
+			achternaam: "Breedveld",
+		}
+	}
+
+	increase = () => {
 		let test = document.getElementById("js--number")
 		test.innerText = parseInt(test.innerText) + 1
 	}
 
-	const decrease = () => {
+	decrease = () => {
 		let test = document.getElementById("js--number")
 		test.innerText = parseInt(test.innerText) - 1
 	}
 
-	return (
-		<>
-			<article className='counter'>
-				<section className='counter__number'>
-					<h1 className='counter__value' id='js--number'>
-						{props.number}
-					</h1>
-				</section>
-				<section className='counter__buttons'>
-					<button onClick={decrease} className='counter__button counter__button--decrease'>
-						Decrease
-					</button>
-					<button onClick={increase} className='counter__button counter__button--increase'>
-						Increase
-					</button>
-				</section>
-			</article>
-		</>
-	)
+	render() {
+		return (
+			<>
+				<article className='counter'>
+					<section className='counter__number'>
+						<h1 className='counter__value' id='js--number'>
+							{this.props.number}
+						</h1>
+					</section>
+					<section className='counter__buttons'>
+						<button onClick={this.decrease} className='counter__button counter__button--decrease'>
+							Decrease
+						</button>
+						<button onClick={this.increase} className='counter__button counter__button--increase'>
+							Increase
+						</button>
+					</section>
+				</article>
+			</>
+		)
+	}
 }
 
 // Exports
